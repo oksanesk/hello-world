ef43907470bc50c3efaa69417b2087d16876d456
Esko Oksanen <esko.oksanen@nic.fi>
2017-10-23 13:06:33 +0300

    Printing all arguments

--- main.c
+++ main.c
@@ -4,5 +4,8 @@
 int main(int argc, char* argv[])
 {
     printf("Number of arguments is %u\n", argc);
+    for (int i=0; i < argc; i++) {
+        printf("Arg %d = %s\n", i, argv[i]);
+    }
     return 0;
 }
